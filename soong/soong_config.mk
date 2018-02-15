ev-staging/android_vendor_ev_build

soong: add non pie support
Change-Id: Ie9e754d5610249b6791c6efff4115d8918d6a682

soong/soong_config.mk
@@ -4,6 +4,7 @@ evervolv_soong:
 	$(hide) (\
 	echo '{'; \
 	echo '"Evervolv": {'; \
+	echo '    "Needs_non_pie_support": $(if $(filter true,$(TARGET_NEEDS_PRELINK_SUPPORT)),true,false),'; \
 	echo '    "Needs_prelink_support": $(if $(filter true,$(TARGET_NEEDS_PRELINK_SUPPORT)),true,false),'; \
 	echo '    "Has_legacy_camera_hal1": $(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false),'; \
 	echo '    "Uses_media_extensions": $(if $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)),true,false),'; \

dirtyunicorns-project/android_vendor_extras

soong/soong_config.mk
@@ -2,6 +2,7 @@ du_soong:
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
+	echo '    "Needs_non_pie_support": $(if $(filter true,$(TARGET_NEEDS_NON_PIE_SUPPORT)),true,false),'; \
 	echo '    "Needs_prelink_support": $(if $(filter true,$(TARGET_NEEDS_PRELINK_SUPPORT)),true,false),'; \
 	echo '    "Has_legacy_camera_hal1": $(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false),'; \
 	echo '    "Uses_media_extensions": $(if $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)),true,false),'; \